145848663e6e1269622ec46508f0acda7ab46d40
ManuCaporaso <manulpda@gmail.com>
2024-07-03 12:41:13 -0300

    modificaciones

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const clientesRouter = require('./routes/clientes');
@@ -8,6 +9,7 @@ const app = express();
 dotenv.config();
 
 // Middleware para manejar JSON y archivos estáticos
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
