3f5ef90533f4d5d607a0b78e649768568c9b312d
ManuCaporaso <manulpda@gmail.com>
2024-07-08 15:01:13 -0300

    cambios

--- db/db.js
+++ db/db.js
@@ -6,8 +6,8 @@ dotenv.config();
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
